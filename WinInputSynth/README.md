64695642a5f18a3ffed2c66ceabe7e9e4cb91e67
EthanC2 <ethanrcox@gmail.com>
2022-07-24 19:35:21 -0500

    Add project files.

---
+++ WinInputSynth/README.md
@@ -0,0 +1,11 @@
+﻿# WinInputSynth
+A Windows input synthesizer.
+
+# Planned Features
+- Choice of type of clicking (Left/Middle/Right and whether to Single/Double click) + Combinations!
+- Tuning for number of clicks per second (or random interval)
+- Choice of where to click (current cursor position or X-/Y-coordinates)
+- Automatic timeout (timed or via number of clicks)
+- Multiple cursors using native OS multithreading
+- Hotkeys (via [accelerators](https://docs.microsoft.com/en-us/windows/win32/learnwin32/accelerator-tables))
+- Recording clicks patterns
---
+++ WinInputSynth/View/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.ApplicationModel.Core;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinInputSynth.View;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    IList<string> mouseButtons, clickModes;
+    /*private readonly List<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
+    {
+    ("mouse", typeof(MousePage)),
+    ("keyboard", typeof(KeyboardPage)),
+    ("mouse+keyboard", typeof(MouseAndKeyboardPage)),
+    ("settings", typeof(SettingsPage)),
+    };*/
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Title = "A Windows Mouse and Keyboard Synthesizer";
+
+        SetTitleBar(AppTitleBar);
+        ExtendsContentIntoTitleBar = true;
+
+        mouseButtons = new List<string>() { "Left", "Right", "Middle" };
+        clickModes = new List<string>() { "Single Click", "Double Click" };
+    }
+
+    //private void myButton_Click(object sender, RoutedEventArgs e)
+    //{
+    //    myButton.Content = "Clicked";
+    //}
+}
